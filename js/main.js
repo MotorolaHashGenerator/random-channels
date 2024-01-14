def7508bd90bc5260641e33778e3c9139c39f47e
Yurii Antonchyk <xsat.php@gmail.com>
2024-01-14 22:06:34 +0200

    Implement random values

--- js/main.js
+++ js/main.js
@@ -12,8 +12,12 @@ function getInputValue(input_id) {
 	return value
 }
 
+function getRandomNumber(min, max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 function getRandElement(elements) {
-	const index = Math.floor(Math.random() * elements.length);
+	const index = getRandomNumber(0, elements.length);
 
 	return elements[index];
 }
@@ -28,15 +32,13 @@ function getRandHash(size, elements) {
 	return hash;
 }
 
-function generateHexadecimalKey(size_input_id) {
-	const size = getInputValue(size_input_id);
+function generateHexadecimal(size) {
 	const elements = ['A', 'B', 'C', 'D', 'E', 'F', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
   
-	alert(getRandHash(size, elements));
+	return getRandHash(size, elements);
 }
 
-function generateRASID(size_input_id) {
-	const size = getInputValue(size_input_id);
+function generateRAS(size) {
 	const elements = [
 		'-', '_', '$', '#', 'A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', 
 		'G', 'g', 'H', 'h', 'I', 'i', 'J', 'j', 'K', 'k', 'L', 'l', 'M', 'm', 'N', 'n', 
@@ -44,9 +46,53 @@ function generateRASID(size_input_id) {
 		'W', 'w', 'X', 'x', 'Y', 'y', 'Z', 'z', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
 	];
   
-	alert(getRandHash(size, elements));
+	return getRandHash(size, elements);
 }
 
 function generateChanel() {
-	confirm("test")
+	const key_id_element = document.getElementById('key_id');
+	const key_id_min = parseInt(key_id_element.getAttribute('min'));
+	const key_id_max = parseInt(key_id_element.getAttribute('max'));
+	key_id_element.value = getRandomNumber(key_id_min, key_id_max);
+
+	const enhanzed_key_element = document.getElementById('enhanzed_key');
+	const enhanzed_key_maxlength = parseInt(enhanzed_key_element.getAttribute('maxlength'));
+	enhanzed_key_element.value = generateHexadecimal(enhanzed_key_maxlength)
+
+	const symmetric_key_element = document.getElementById('symmetric_key');
+	const symmetric_key_maxlength = parseInt(symmetric_key_element.getAttribute('maxlength'));
+	symmetric_key_element.value = generateHexadecimal(symmetric_key_maxlength)
+
+	const ras_id_element = document.getElementById('ras_id');
+	const ras_id_maxlength = parseInt(ras_id_element.getAttribute('maxlength'));
+	ras_id_element.value = generateRAS(ras_id_maxlength)
+
+	const call_id_element = document.getElementById('call_id');
+	const call_id_min = parseInt(call_id_element.getAttribute('min'));
+	const call_id_max = parseInt(call_id_element.getAttribute('max'));
+	call_id_element.value = getRandomNumber(call_id_min, call_id_max);
+
+	const color_code_element = document.getElementById('color_code');
+	const color_code_min = parseInt(color_code_element.getAttribute('min'));
+	const color_code_max = parseInt(color_code_element.getAttribute('max'));
+	color_code_element.value = getRandomNumber(color_code_min, color_code_max);
+
+	const repeter_time_slot_element = document.getElementById('repeter_time_slot');
+	const repeter_time_slot_min = parseInt(repeter_time_slot_element.getAttribute('min'));
+	const repeter_time_slot_max = parseInt(repeter_time_slot_element.getAttribute('max'));
+	repeter_time_slot_element.value = getRandomNumber(repeter_time_slot_min, repeter_time_slot_max);
+
+	const tx_frequency_element = document.getElementById('tx_frequency');
+	const tx_frequency_element_min = parseInt(tx_frequency_element.getAttribute('data-min'));
+	const tx_frequency_element_max = parseInt(tx_frequency_element.getAttribute('data-max'));
+	tx_frequency_element.value = getRandomNumber(tx_frequency_element_min, tx_frequency_element_max);
+	tx_frequency_element.value += "."
+	tx_frequency_element.value += getRandomNumber(0, 999)
+
+	const rx_frequency_element = document.getElementById('rx_frequency');
+	const rx_frequency_element_min = parseInt(rx_frequency_element.getAttribute('data-min'));
+	const rx_frequency_element_max = parseInt(rx_frequency_element.getAttribute('data-max'));
+	rx_frequency_element.value = getRandomNumber(rx_frequency_element_min, rx_frequency_element_max);
+	rx_frequency_element.value += "."
+	rx_frequency_element.value += getRandomNumber(0, 999)
 }

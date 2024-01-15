fa33bac9abe30b8df55ebbb5eb1a0aa6d32945cc
Yurii Antonchyk <xsat.php@gmail.com>
2024-01-15 20:23:41 +0200

    Update random frequency

--- js/main.js
+++ js/main.js
@@ -82,17 +82,19 @@ function generateChanel() {
 	const repeter_time_slot_max = parseInt(repeter_time_slot_element.getAttribute('max'));
 	repeter_time_slot_element.value = getRandomNumber(repeter_time_slot_min, repeter_time_slot_max);
 
+	const random_number = getRandElement([9, 99, 99])
+
 	const tx_frequency_element = document.getElementById('tx_frequency');
 	const tx_frequency_element_min = parseInt(tx_frequency_element.getAttribute('data-min'));
 	const tx_frequency_element_max = parseInt(tx_frequency_element.getAttribute('data-max'));
 	tx_frequency_element.value = getRandomNumber(tx_frequency_element_min, tx_frequency_element_max);
 	tx_frequency_element.value += "."
-	tx_frequency_element.value += getRandomNumber(0, 999)
+	tx_frequency_element.value += getRandomNumber(0, random_number)
 
 	const rx_frequency_element = document.getElementById('rx_frequency');
 	const rx_frequency_element_min = parseInt(rx_frequency_element.getAttribute('data-min'));
 	const rx_frequency_element_max = parseInt(rx_frequency_element.getAttribute('data-max'));
 	rx_frequency_element.value = getRandomNumber(rx_frequency_element_min, rx_frequency_element_max);
 	rx_frequency_element.value += "."
-	rx_frequency_element.value += getRandomNumber(0, 999)
+	rx_frequency_element.value += getRandomNumber(0, random_number)
 }
